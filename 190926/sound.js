20966eeddd32dbbe99bb8eefac1b15394571509e
daeun <leede0418@likelion.org>
2019-10-01 03:11:59 +0900

    feat: Sense whether there is some sound or not

---
+++ 190926/sound.js
@@ -0,0 +1,24 @@
+const gpio = require('node-wiring-pi');
+const SOUND = 29;
+var count = 0;
+
+const DetectSound = function()
+{
+    let data = gpio.digitalRead(SOUND);
+    if(data)
+    {
+        console.log("%d !", count++);
+    }
+    setTimeout(DetectSound, 10);
+}
+
+process.on('SIGINT', function()
+{
+    console.log("Program Exit...");
+    process.exit();
+})
+
+gpio.wiringPiSetup();
+gpio.pinMode(SOUND, gpio.INPUT);
+console.log("소리탐지 중...");
+setTimeout(DetectSound, 10);
